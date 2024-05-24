3856e2180ce8ebec6a2afe4abc0e9571d244f844
kmartinezm <keynermrtz@gmail.com>
2024-05-24 15:40:30 -0500

    Unidad 6 Version 51

--- mynewbook/_build/jupyter_execute/und4_forecast.py
+++ mynewbook/_build/jupyter_execute/und4_forecast.py
@@ -38,6 +38,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+# #### 2.3.1.1 Recopilación de los datos
+# 
+# En este bloque, se carga el conjunto de datos desde un archivo CSV utilizando la biblioteca pandas. Se especifica el delimitador de campos y se visualizan las primeras filas del dataframe para verificar que los datos se han cargado correctamente.
+
 # In[2]:
 
 
@@ -45,6 +49,10 @@ import seaborn as sns
 df_data = pd.read_csv('./dataset.csv',sep=';')
 df_data.head()
 
+
+# #### 2.3.1.2 Transformación de Datos
+# 
+# Este bloque define una función para transformar el dataframe original. Se realiza una copia del dataframe, se convierte la columna de fechas a un formato de fecha adecuado, y se agregan nuevas columnas para el año, mes y día. Luego, se seleccionan y reorganizan las columnas, y finalmente, se pivotea la tabla para facilitar su uso en análisis posteriores.
 
 # In[3]:
 
@@ -83,6 +91,10 @@ df = trf_data(df_data)
 df.head()
 
 
+# #### 2.3.1.3 Remuestreo de la serie temporal
+# 
+# En este bloque, se crea una copia del dataframe transformado y se remuestrean los datos a valores mensuales. Se agrupan los datos por fecha y se calcula la media mensual. Luego, se seleccionan las columnas relevantes y se visualiza el nuevo dataframe remuestreado.
+
 # In[5]:
 
 
@@ -99,6 +111,14 @@ df_vcm = df_vcm[['fechaoperacion','valor']]
 df_vcm.head()
 
 
+# ### 2.3.2 Visualización y Creación de la Serie Temporal
+# 
+# Se realizarán dos tareas fundamentales: la visualización de la serie temporal y la estimación de su tendencia mediante técnicas de suavizamiento, así como la creación de una serie temporal para el análisis con la metodología Holt-Winters.
+# 
+# #### 2.3.2.1 Visualización y Suavizamiento de la Serie Temporal
+# 
+# Primero, se grafica la serie de tiempo con los datos remuestreados a valores mensuales. Se utiliza la técnica de suavizamiento LOWESS (Locally Weighted Scatterplot Smoothing) para estimar la tendencia de la serie temporal. LOWESS es un método no paramétrico que ajusta múltiples regresiones locales, proporcionando una estimación suave de la tendencia subyacente.
+
 # In[6]:
 
 
@@ -113,39 +133,51 @@ plt.ylabel('Precio de bolsa mensual')
 plt.legend()
 plt.show()
 
-# Crear una serie temporal
-Ventas_ts = pd.Series(df_vcm['valor'].values, index=pd.date_range(start='2006-01', periods=len(df_vcm), freq='M'))
-print(Ventas_ts)
 
+# ### 2.3.3 Aplicación de la metodología Holt-Winters
+# 
+# En esta sección, se detalla la aplicación de la metodología Holt-Winters para el pronóstico de series temporales. Este método es particularmente útil para series con tendencia y estacionalidad. A continuación, se justifica cada procedimiento y se presenta el código correspondiente.
+# 
+# #### 2.3.3.1 Creación de la Serie Temporal
+# 
+# A continuación, se crea una serie temporal utilizando los valores mensuales transformados. Esto es esencial para aplicar modelos de pronóstico, como la metodología Holt-Winters. Holt-Winters es una técnica de suavizamiento exponencial que considera la tendencia y la estacionalidad de los datos, y es particularmente útil para series temporales con patrones estacionales y tendencias
+# 
+# El boxplot mensual ayuda a identificar patrones estacionales y posibles anomalías en los datos. Esta visualización es crucial para decidir sobre el tratamiento de la estacionalidad en el modelo Holt-Winters.
 
 # In[7]:
 
 
+# Crear una serie temporal
+precio_ts = pd.Series(df_vcm['valor'].values, index=pd.date_range(start='2006-01', periods=len(df_vcm), freq='M'))
+
 # Crear un boxplot de los datos
 plt.figure(figsize=(10, 8))
-sns.boxplot(x=Ventas_ts.index.month, y=Ventas_ts.values,color='skyblue')
+sns.boxplot(x=precio_ts.index.month, y=precio_ts.values,color='skyblue')
 plt.xlabel('Mes')
 plt.ylabel('Valor')
 plt.title('Boxplot de los valores mensuales')
 plt.show()
 
 
+# #### 2.3.3.2 Estabilización de la Variabilidad
+# 
+# Para estabilizar la variabilidad, se aplica una transformación logarítmica a la serie temporal. Luego, se ajusta un modelo Holt-Winters que considera tanto la tendencia como la estacionalidad aditivas.
+# 
+# La transformación logarítmica se utiliza para estabilizar la variabilidad y hacer que los datos sean más apropiados para el modelado. El método Holt-Winters se elige por su capacidad para manejar tanto la tendencia como la estacionalidad, factores identificados en la serie temporal. La impresión de los parámetros del modelo proporciona transparencia y permite verificar el ajuste del modelo.
+
 # In[8]:
 
 
 # Estabilización de la variabilidad
-VentasLog = np.log(Ventas_ts)
+preciolog = np.log(precio_ts)
 
 # Aplicación del método Holt-Winters
-model = ExponentialSmoothing(VentasLog, seasonal_periods=12, trend='add', seasonal='add')
+model = ExponentialSmoothing(preciolog, seasonal_periods=12, trend='add', seasonal='add')
 fit = model.fit()
-
-# Imprimir los parámetros del modelo
-print(fit.params)
 
 # Graficar los resultados
 plt.figure(figsize=(12, 8))
-plt.plot(VentasLog, label='Observado')
+plt.plot(preciolog, label='Observado')
 plt.plot(fit.fittedvalues, label='Predicho')
 plt.title("Estimación Holt-Winters")
 plt.xlabel('Año')
@@ -154,17 +186,23 @@ plt.legend()
 plt.show()
 
 
+# #### 2.3.3.3 Predicción del Modelo
+# 
+# Finalmente, se utiliza el modelo ajustado para predecir los valores de la serie temporal para el próximo año (12 meses). Se grafican las observaciones originales, los valores ajustados por el modelo, y las predicciones.
+# 
+# La predicción para el próximo año permite evaluar la capacidad del modelo para generalizar y prever futuros valores. Graficar las observaciones originales junto con los valores ajustados y predichos facilita la comparación visual y la validación del modelo.
+
 # In[9]:
 
 
 # Predicción del siguiente año
-forecast_next_year = fit.forecast(steps=24)
+forecast_next_year = fit.forecast(steps=12)
 
 # Graficar la predicción
 plt.figure(figsize=(12, 8))
-plt.plot(VentasLog, label='Observado')
+plt.plot(preciolog, label='Observado')
 plt.plot(fit.fittedvalues, label='Entrenado')
-plt.plot(pd.date_range(start='2024-01', periods=24, freq='M'), forecast_next_year, label='Predicción')
+plt.plot(pd.date_range(start='2024-01', periods=12, freq='M'), forecast_next_year, label='Predicción')
 plt.title("Predicción del siguiente año")
 plt.xlabel('Año')
 plt.ylabel('Observado/predicho')
@@ -172,26 +210,30 @@ plt.legend()
 plt.show()
 
 
+# ### 2.3.4 Evalaución del Modelo
+# 
+# En esta sección, se evalúa el desempeño del modelo Holt-Winters utilizando dos métricas comunes: el Error Cuadrático Medio (MSE) y el Error Absoluto Medio (MAE). Estas métricas proporcionan una medida cuantitativa de qué tan bien se ajustan las predicciones del modelo a los datos reales.
+# 
+# Error Cuadrático Medio (MSE)
+# El Error Cuadrático Medio (MSE) calcula el promedio de los errores al cuadrado entre las predicciones y los datos reales. Un MSE más bajo indica un mejor ajuste del modelo a los datos observados.
+# 
+# Error Absoluto Medio (MAE)
+# El Error Absoluto Medio (MAE) calcula el promedio de las diferencias absolutas entre las predicciones y los datos reales. Es una medida de la magnitud promedio de los errores en las predicciones del modelo.
+# 
+# **Resultados de la Evaluación:**
+# 
+# - Error Cuadrático Medio (MSE): 0.2243
+# - Error Absoluto Medio (MAE): 0.3502
+# 
+# Estos valores proporcionan una indicación del rendimiento del modelo Holt-Winters en la predicción de los últimos 12 meses de la serie temporal. Un MSE y MAE más bajos indican un mejor ajuste del modelo a los datos de prueba.
+
 # In[10]:
-
-
-forecast_next_year
-
-
-# In[11]:
-
-
-predict_pbna = np.exp(forecast_next_year)
-predict_pbna
-
-
-# In[12]:
 
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Selecciona un conjunto de datos de prueba
-test_data = VentasLog[-12:]  # Supongamos que los últimos 12 meses son el conjunto de prueba
+test_data = preciolog[-12:]  # Supongamos que los últimos 12 meses son el conjunto de prueba
 
 # Realiza la predicción para estos datos de prueba
 forecast_next_year = fit.forecast(steps=12)
@@ -205,3 +247,17 @@ mae = mean_absolute_error(test_data, forecast_next_year)
 print("Error Cuadrático Medio (MSE):", mse)
 print("Error Absoluto Medio (MAE):", mae)
 
+
+# ### 2.3.5 Conclusión
+# 
+# Se realizó un análisis de una serie temporal utilizando la metodología Holt-Winters. Se han aplicado técnicas de preparación de datos, suavizamiento y modelado para comprender y predecir los patrones en los datos de precios de bolsa mensuales.
+# 
+# La aplicación de la metodología Holt-Winters se justifica plenamente por la presencia de estacionalidad y tendencia en los datos. Cada paso del proceso, desde la visualización inicial hasta la predicción, es crucial para garantizar un modelo robusto y transparente. Este enfoque sistemático no solo mejora la precisión del modelo, sino que también proporciona una base sólida para la interpretación y toma de decisiones basadas en los resultados del análisis. 
+# 
+# El análisis demuestra cómo un enfoque estructurado y bien justificado en la preparación y modelado de datos temporales puede conducir a resultados precisos y útiles, permitiendo una comprensión profunda de las dinámicas subyacentes en la serie temporal y proporcionando herramientas valiosas para la predicción y planificación futura.
+# 
+# Tras la evaluación del modelo Holt-Winters, se ha encontrado que presenta un buen ajuste a los datos, como se evidencia en los resultados de las métricas de evaluación. Con un Error Cuadrático Medio (MSE) de 0.2243 y un Error Absoluto Medio (MAE) de 0.3502, el modelo logra predecir con precisión los valores futuros de la serie temporal.
+# 
+# Estos hallazgos sugieren que el modelo Holt-Winters es una herramienta efectiva para la predicción de series temporales con patrones estacionales y tendencias.
+# 
+# Este estudio proporciona una sólida base para comprender y predecir la dinámica de los precios de bolsa mensuales, lo que puede ser de gran utilidad para la toma de decisiones.
--- mynewbook/_build/jupyter_execute/und5_forecast.py
+++ mynewbook/_build/jupyter_execute/und5_forecast.py
@@ -41,6 +41,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+# #### 5.3.1.1 Recopilación de los datos
+# 
+# En este bloque, se carga el conjunto de datos desde un archivo CSV utilizando la biblioteca pandas. Se especifica el delimitador de campos y se visualizan las primeras filas del dataframe para verificar que los datos se han cargado correctamente.
+
 # In[2]:
 
 
@@ -48,6 +52,10 @@ import seaborn as sns
 df_data = pd.read_csv('./dataset.csv',sep=';')
 df_data.head()
 
+
+# #### 5.3.1.2 Transformación de Datos
+# 
+# Este bloque define una función para transformar el dataframe original. Se realiza una copia del dataframe, se convierte la columna de fechas a un formato de fecha adecuado, y se agregan nuevas columnas para el año, mes y día. Luego, se seleccionan y reorganizan las columnas, y finalmente, se pivotea la tabla para facilitar su uso en análisis posteriores.
 
 # In[3]:
 
@@ -86,6 +94,10 @@ df = trf_data(df_data)
 df.head()
 
 
+# #### 5.3.1.3 Remuestreo de la serie temporal
+# 
+# En este bloque, se crea una copia del dataframe transformado y se remuestrean los datos a valores mensuales. Se agrupan los datos por fecha y se calcula la media mensual. Luego, se seleccionan las columnas relevantes y se visualiza el nuevo dataframe remuestreado.
+
 # In[5]:
 
 
@@ -102,7 +114,32 @@ df_vcm = df_vcm[['fechaoperacion','valor']]
 df_vcm.head()
 
 
+# ### 5.3.2 Validación Estacionariedad
+# 
+# La primera etapa de este análisis es la validación de estacionariedad de la serie temporal. La estacionariedad es un requisito fundamental para la modelización ARIMA, ya que asegura que las propiedades estadísticas de la serie no cambien con el tiempo. A continuación, se presentan los resultados obtenidos y se visualiza la tendencia de la serie temporal.
+# 
+# Para verificar la estacionariedad de la serie, se aplicó la prueba de Dickey-Fuller Aumentada (ADF). Los resultados obtenidos se muestran a continuación:
+
 # In[6]:
+
+
+# estacionario
+adf = adfuller(df_vcm['valor'],maxlag=1)
+print('El T-Test es: ',adf[0])
+print('El p-value es: ',adf[1])
+print('Valores criticos: ',adf[4])
+
+
+# El valor del T-Test es -4.283, el cual es menor que los valores críticos en todos los niveles de significancia (1%, 5% y 10%). Específicamente:
+# 
+# - Nivel de significancia del 1%: El valor crítico es -3.461. Dado que -4.283 < -3.461, rechazamos la hipótesis nula con un nivel de confianza del 99%.
+# - Nivel de significancia del 5%: El valor crítico es -2.875. Dado que -4.283 < -2.875, rechazamos la hipótesis nula con un nivel de confianza del 95%.
+# - Nivel de significancia del 10%: El valor crítico es -2.573. Dado que -4.283 < -2.573, rechazamos la hipótesis nula con un nivel de confianza del 90%.
+# - El p-value obtenido (0.000475) es considerablemente menor que el umbral común de 0.05, lo que refuerza nuestra decisión de rechazar la hipótesis nula de no estacionariedad.
+# 
+# La serie temporal del precio de bolsa mensual es estacionaria. Este resultado valida que las propiedades estadísticas de la serie (como la media y la varianza) permanecen constantes en el tiempo, cumpliendo así con uno de los requisitos clave para la modelización ARIMA.
+
+# In[7]:
 
 
 # Graficar la serie de tiempo y su tendencia estimada
@@ -125,110 +162,190 @@ plt.legend()
 plt.show()
 
 
-# In[7]:
-
-
-df_vcm.head()
-
+# ### 5.3.3 Autocorrelación (ACF) y Autorcorrelación Parcial (PCAF)
+# 
+# Después de validar la estacionariedad de la serie temporal del precio de bolsa mensual, el siguiente paso en la metodología de Box-Jenkins es analizar la autocorrelación (ACF) y la autocorrelación parcial (PACF) de la serie. Este análisis nos ayuda a identificar el comportamiento y la estructura de dependencia temporal en los datos, facilitando la selección de los parámetros para los modelos ARIMA.
+# 
+# Para calcular y graficar las funciones de autocorrelación (ACF) y autocorrelación parcial (PACF), se utilizó el siguiente código:
 
 # In[8]:
 
 
-# creando nuevo dataframe para analizar boxplot mensual
+# Graficar ACF y PACF para la serie diferenciada
 
-# creando copia del dataframe transformado
-df_mes = df_vcm.copy()
+# creando lienzo
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
 
-df_mes['mes'] = df_mes.apply(lambda x: x['fechaoperacion'].month,axis=1)
+# realizando la autocorrelación
+plot_acf(df_vcm['valor'].dropna(), lags=40, ax=ax1)
 
-df_mes = df_mes[['mes','valor']]
+# realiznado la autocorrelación parcial
+plot_pacf(df_vcm['valor'].dropna(), lags=40, ax=ax2)
 
+# mostrando gráfico
+plt.show()
+
+
+# - **Autocorrelación (ACF):**
+# El gráfico de la ACF muestra la correlación entre la serie temporal y sus rezagos hasta 40 períodos. La ACF es útil para identificar la presencia de componentes de media móvil (MA).
+# 
+# Observamos que las autocorrelaciones son significativas hasta el rezago 6, lo que sugiere que un modelo de media móvil de orden 6 (MA(6)) podría ser adecuado para capturar la dependencia temporal en los datos.
+# 
+# - **Autocorrelación Parcial (PACF):**
+# El gráfico de la PACF muestra la correlación parcial entre la serie temporal y sus rezagos, eliminando el efecto de los rezagos intermedios. La PACF es útil para identificar la presencia de componentes autoregresivos (AR).
+# 
+# Observamos que las autocorrelaciones parciales son significativas hasta el rezago 4, lo que sugiere que un modelo autoregresivo de orden 4 (AR(4)) podría ser adecuado para capturar la dependencia temporal en los datos.
+# 
+# Después de validar la estacionariedad de la serie temporal del precio de bolsa mensual y analizar las funciones de autocorrelación (ACF) y autocorrelación parcial (PACF), se procedió a la búsqueda del mejor modelo ARIMA. Aunque la serie temporal fue determinada como estacionaria, se identificó que presentaba una estacionariedad débil. Por lo tanto, se realizó una diferenciación adicional para mejorar la modelización.
+# 
+# Para encontrar el mejor modelo ARIMA, se utilizó la función auto_arima de la biblioteca pmdarima, que selecciona automáticamente el modelo más adecuado basado en criterios de información y pruebas estadísticas. El código utilizado es el siguiente:
 
 # In[9]:
 
 
-# Calcular la serie diferenciada
-df_vcm['valor_dif'] = df_vcm['valor'].diff().dropna()
+from pmdarima.arima import auto_arima
+import pandas as pd
 
+# Suponiendo que dif_Indice_ts es tu serie temporal diferenciada
+# Ajustar el modelo ARIMA automáticamente
+modelo = auto_arima(df_vcm['valor'], seasonal=False, stepwise=True, suppress_warnings=True)
+
+# Mostrar el resumen del modelo ajustado
+print(modelo.summary())
+
+
+# - **Modelo SARIMAX(4, 1, 3):** Este modelo indica que el mejor ajuste para los datos se obtiene con un modelo autoregresivo de orden 4 (AR(4)), una diferenciación de orden 1, y una media móvil de orden 3 (MA(3)). La diferenciación de orden 1 se realizó para asegurar una mejor estacionariedad, dado que la serie original presentaba una estacionariedad débil.
+# 
+# - **Coeficientes y Significancia:** Los coeficientes de los términos AR y MA son estadísticamente significativos, como lo indica el valor de p (P>|z|) menor a 0.05 para cada coeficiente. Esto sugiere que los términos incluidos en el modelo son relevantes para explicar la variabilidad en los datos.
+# 
+# El análisis detallado de la autocorrelación y autocorrelación parcial, seguido de la búsqueda automática del mejor modelo ARIMA, permitió identificar el modelo SARIMAX(4, 1, 3) como el más adecuado para la serie temporal del precio de bolsa mensual. Este modelo tiene en cuenta la estructura temporal y la necesidad de diferenciación para alcanzar una mejor estacionariedad, garantizando una mayor precisión en las predicciones futuras.
+
+# ### 5.3.4 Modelo ARIMA
+# 
+# Tras validar la estacionariedad de la serie temporal y analizar las funciones de autocorrelación (ACF) y autocorrelación parcial (PACF), se procedió a la modelización utilizando la metodología ARIMA. Se ajustaron y evaluaron dos modelos: SARIMAX(1, 0, 1) y SARIMAX(4, 1, 3). A continuación, se presentan los resultados obtenidos y su interpretación.
+# 
+# Para ajustar los modelos, se emplearon los siguientes códigos:
 
 # In[10]:
 
 
-# Graficar ACF y PACF para la serie diferenciada
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
-plot_acf(df_vcm['valor_dif'].dropna(), lags=40, ax=ax1)
-plot_pacf(df_vcm['valor_dif'].dropna(), lags=40, ax=ax2)
-plt.show()
-
-
-# ## Gráficos de Autocorrelación (ACF) y Autocorrelación Parcial (PACF) para la serie de tiempo diferenciada
-# 
-# El gráfico ACF muestra que las autocorrelaciones se reducen rápidamente, lo cual es típico en un proceso que podría ajustarse bien con un modelo ARIMA.
-# 
-# El gráfico PACF muestra un corte brusco después del primer *lag*, lo que sugiere que un modelo AR(1) podría ser adecuado.
-# 
-# Estos gráficos son útiles para determinar los parámetros \(p\) y \(q\) del modelo ARIMA:
-# 
-# - \(p\) (AR): El número de *lags* que muestran correlaciones significativas en el PACF antes de cortar. En este caso, \(p\) podría ser 1.
-# - \(q\) (MA): El número de *lags* con correlaciones significativas en el ACF antes de que caiga hacia cero. Aquí, \(q\) podría ser también 1.
-
-# In[11]:
-
-
 # Ajustar un modelo ARIMA(1,1,1) a la serie temporal, usando los parámetros sugeridos por los gráficos ACF y PACF
-model = ARIMA(df_vcm['valor'], order=(1, 1, 1))
+model = ARIMA(df_vcm['valor'], order=(1, 0, 1))
 model_fit = model.fit()
 
 # Mostrar el resumen del modelo ajustado
 model_fit.summary()
 
 
-# ## Resumen del modelo ARIMA(1,1,1) ajustado a la serie de tiempo
+# - **Coeficientes:** El término AR(1) es significativo (p < 0.05), mientras que el término MA(1) no lo es.
 # 
-# El modelo ARIMA(1,1,1) ha sido ajustado nuevamente a la serie de tiempo con los siguientes resultados en el resumen:
+# - **AIC/BIC:** Los valores de AIC (2601.744) y BIC (2615.245) nos indican la calidad del ajuste del modelo, siendo AIC un indicador crucial para la selección de modelos.
+
+# In[11]:
+
+
+# Ajustar un modelo ARIMA(4,1,3) a la serie temporal, usando los parámetros sugeridos por los gráficos ACF y PACF
+model_vf = ARIMA(df_vcm['valor'].dropna(), order=(4, 1, 3))
+model_fit_vf = model_vf.fit()
+
+# Mostrar el resumen del modelo ajustado
+model_fit_vf.summary()
+
+
+# - **Coeficientes:** Todos los términos AR y MA son significativos (p < 0.05), indicando que estos términos son relevantes para el modelo.
 # 
-# - **Coeficiente AR1:** 0.7581
-# - **Coeficiente MA1:** -0.9530
-# - **Log Likelihood:** -1290.861
-# - **AIC:** 2587.721
-# - **BIC:** 2597.833
+# - **AIC/BIC:** Los valores de AIC (2559.179) y BIC (2586.144) son menores comparados con el modelo ARIMA(1, 0, 1), sugiriendo un mejor ajuste del modelo.
 # 
-# Los coeficientes son significativos y el modelo parece ser adecuado dadas las estadísticas proporcionadas. 
-# Se escoge un modelo ARIMA(1,1,1) cuando la evidencia inicial (como el análisis de las funciones de autocorrelación y autocorrelación parcial) sugiere que estos componentes son suficientes para modelar los datos de manera adecuada. En este caso particular, el modelo ARIMA(1,1,1) fue sugerido inicialmente basado en la observación de los gráficos ACF y PACF, y confirmado por su desempeño en la adaptación de los datos históricos.
 # 
-# Este modelo es particularmente útil cuando se desea un modelo simple que aún sea capaz de capturar las dependencias clave en los datos sin sobreajustar o complicar demasiado el modelo, permitiendo realizar predicciones efectivas y entender la dinámica de la serie de tiempo.
+# Comparando los dos modelos ajustados, el modelo SARIMAX(4, 1, 3) presenta un mejor ajuste en términos de AIC y BIC, y todos sus coeficientes son significativos. 
+# 
+# Este análisis refuerza la elección del modelo SARIMAX(4, 1, 3) como el más adecuado para la serie temporal del precio de bolsa mensual, proporcionando una base sólida para las predicciones y análisis futuros en la metodología de Box-Jenkins.
+
+# ### 5.3.5 Evaluación del modelo
+# 
+# - **Prueba de Ljung-Box:** La prueba de Ljung-Box evalúa si hay autocorrelación significativa en los residuos de un modelo ARIMA. Los resultados que has obtenido son los siguientes:
 
 # In[12]:
 
 
-# Hacer predicciones con el modelo ARIMA ajustado para los próximos 24 períodos
-forecast_24 = model_fit.get_forecast(steps=24)
-forecast_conf_int_24 = forecast_24.conf_int()
+from statsmodels.stats.diagnostic import acorr_ljungbox
 
-# Crear un DataFrame para visualizar las predicciones y los intervalos de confianza para 24 meses
-forecast_df_24 = pd.DataFrame({
-    'Predicted': forecast_24.predicted_mean,
-    'Lower CI': forecast_conf_int_24.iloc[:, 0],
-    'Upper CI': forecast_conf_int_24.iloc[:, 1]
-})
+# Obtener los residuos del modelo ajustado
+residuals = model_fit_vf.resid
 
-forecast_df_24
+# Prueba de Ljung-Box para autocorrelación en los residuos
+ljung_box_result = acorr_ljungbox(residuals.dropna(), lags=[10, 20, 30], return_df=True)
+print('Prueba de Ljung-Box:')
+print(ljung_box_result)
 
+
+# Los altos p-valores indican que no se rechaza la hipótesis nula de que no hay autocorrelación en los residuos a esos rezagos. En otras palabras, no hay evidencia significativa de autocorrelación en los residuos a los rezagos 10, 20, y 30. Esto sugiere que los residuos se comportan como ruido blanco, lo que es deseable y sugiere que el modelo ARIMA ajustado es adecuado.
+# 
+# - **Estadístico de Durbin-Watson:** El estadístico de Durbin-Watson se utiliza para detectar la autocorrelación de primer orden en los residuos de un modelo de regresión:
 
 # In[13]:
 
 
+from statsmodels.stats.stattools import durbin_watson
+
+# Prueba de Durbin-Watson para autocorrelación en los residuos
+dw_stat = durbin_watson(residuals)
+print(f'Estadístico de Durbin-Watson: {dw_stat}')
+
+
+# El valor del estadístico de Durbin-Watson varía entre 0 y 4:
+# 
+# - Un valor alrededor de 2 sugiere que no hay autocorrelación de primer orden en los residuos.
+# - Un valor cercano a 0 sugiere autocorrelación positiva.
+# - Un valor cercano a 4 sugiere autocorrelación negativa.
+# 
+# En este caso, el estadístico de Durbin-Watson es aproximadamente 2, lo que indica que no hay autocorrelación significativa de primer orden en los residuos. Esto es consistente con los resultados de la prueba de Ljung-Box y sugiere que los residuos son efectivamente ruido blanco.
+# 
+# Los resultados de la prueba de Ljung-Box y el estadístico de Durbin-Watson sugieren que los residuos del modelo ARIMA(4,1,3) no presentan autocorrelación significativa y se comportan como ruido blanco.
+
+# ### 5.3.6 Prónostico ARIMA
+# 
+# Luego de ajustar el modelo SARIMAX(4, 1, 3) como el más adecuado para la serie temporal del precio de bolsa mensual, se realizaron predicciones para los próximos 12 meses. Este pronóstico es crucial para la planificación y toma de decisiones estratégicas en el mercado de energía.
+# 
+# Para generar las predicciones y visualizar los resultados, se utilizaron los siguientes bloques de código:
+
+# In[14]:
+
+
+# Hacer predicciones con el modelo ARIMA ajustado para los próximos 12 períodos
+forecast_12 = model_fit_vf.get_forecast(steps=12)
+forecast_conf_int_12 = forecast_12.conf_int()
+
+# Crear un DataFrame para visualizar las predicciones y los intervalos de confianza para 24 meses
+forecast_df_12 = pd.DataFrame({
+    'Predicted': forecast_12.predicted_mean,
+    'Lower CI': forecast_conf_int_12.iloc[:, 0],
+    'Upper CI': forecast_conf_int_12.iloc[:, 1]
+})
+
+forecast_df_12
+
+
+# In[15]:
+
+
 # Generar fechas de predicción para 24 meses
-prediction_dates_24 = pd.date_range(start=df_vcm['fechaoperacion'].iloc[-1] + pd.DateOffset(months=1), periods=24, freq='M')
+prediction_dates_12 = pd.date_range(start=df_vcm['fechaoperacion'].iloc[-1] + pd.DateOffset(months=1), periods=12, freq='M')
 
 # Graficar la serie original y las predicciones para 24 meses
 plt.figure(figsize=(14, 7))
 plt.plot(df_vcm['fechaoperacion'], df_vcm['valor'], label='Datos originales', marker='o')
-plt.plot(prediction_dates_24, forecast_24.predicted_mean, label='Predicciones 24 meses', marker='o', linestyle='--')
-plt.title('Predicciones de Serie de Tiempo con ARIMA(1,1,1) para 24 Meses')
+plt.plot(prediction_dates_12, forecast_12.predicted_mean, label='Predicciones 24 meses', marker='o', linestyle='--')
+plt.title('Predicciones de Serie de Tiempo con ARIMA(4,1,3) para 24 Meses')
 plt.xlabel('Fecha')
 plt.ylabel('Valor')
 plt.legend()
 plt.grid(True)
 plt.show()
 
+
+# - La gráfica muestra la serie temporal original y las predicciones para los próximos 12 meses.
+# - Las predicciones se visualizan con una línea de puntos, y los intervalos de confianza están representados por una sombra alrededor de las predicciones.
+# - Esto proporciona una representación visual clara de la tendencia futura esperada del precio de bolsa mensual y la incertidumbre asociada a estas predicciones.
+# 
+# El modelo SARIMAX(4, 1, 3) ha proporcionado un pronóstico robusto para los próximos 12 meses del precio de bolsa de energía. Las predicciones y sus intervalos de confianza permiten una evaluación detallada de las posibles variaciones futuras, proporcionando información valiosa para la planificación y toma de decisiones estratégicas en el mercado de energía.
